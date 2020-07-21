c6c5022c1bcc00bb3e79fed4e4d49568fe06c771
yji26 <yji26@ncsu.edu>
2020-07-20 23:40:14 -0400

    Updated exploring data tabs.

--- server.R
+++ server.R
@@ -12,18 +12,21 @@ library(rgl)
 library(tree)
 library(plotly)
 library(ggfortify)
+library(htmlwidgets)
 
 shinyServer(function(input, output, session) {
-  
+  #Read in and label the data
   wdbc <- read_csv("wdbc.data", col_names = FALSE)
   colnames(wdbc) <- c("id", "diagnosis", 
                       "radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean", "compactness_mean", "concavity_mean", "concavepoints_mean", "symmetry_mean", "fractal_mean",
                       "radius_se", "texture_se", "perimeter_se", "area_se", "smoothness_se", "compactness_se", "concavity_se", "concavepoints_se", "symmetry_se", "fractal_se",
                       "radius_worst", "texture_worst", "perimeter_worst", "area_worst", "smoothness_worst", "compactness_worst", "concavity_worst", "concavepoints_worst", "symmetry_worst", "fractal_worst"
   )
-  
+  wdbc$id <- as.character(wdbc$id)
   wdbc$diagnosis <- as.factor(wdbc$diagnosis)
+  #wdbc <- wdbc %>% arrange(diagnosis)
   
+  #Render raw data table
   output$raw_table <- renderTable(wdbc)
   output$downloadRaw <- downloadHandler(
     filename = "WDBCRawData.csv",
@@ -32,6 +35,7 @@ shinyServer(function(input, output, session) {
     }
   )
   
+  #Render summary table
   output$summary_table <- renderTable(apply(wdbc[, 3:32], 2, summary), rownames = TRUE)
   output$downloadSummary <- downloadHandler(
     filename = "WDBCSummaryData.csv",
@@ -40,10 +44,60 @@ shinyServer(function(input, output, session) {
     }
   )
 
+  #Render boxplot
   output$boxplot <- renderPlotly({
     g <- ggplot(data = wdbc, aes(x = diagnosis, y = eval(as.symbol(input$box_input)))) + 
       geom_boxplot() + labs(title = paste0("Boxplot for ", input$box_input, " measurements \nbetween Benign and Malignant diagnoses")) + 
       labs(y = input$box_input)
-    ggplotly(g)
+    g
   })
+  
+  output$downloadBoxplotData <- downloadHandler(
+    filename = "WDBCBoxplotData.csv",
+    content = function(file) {
+      write.csv(wdbc[, c("diagnosis", input$box_input)], file, row.names = FALSE)
+    }
+  )
+  
+  #Render scatterplot
+  output$scatterplot <- renderPlotly({
+    if (input$color_diag_scatter) {
+      g <- ggplot(data = wdbc, aes(x = eval(as.symbol(input$scatter_x)), y = eval(as.symbol(input$scatter_y)), color = diagnosis))
+    } else {
+      g <- ggplot(data = wdbc, aes(x = eval(as.symbol(input$scatter_x)), y = eval(as.symbol(input$scatter_y))))
+    }
+    ggplotly(g + geom_point() + 
+                 labs(title = paste0("Scatterplot for ", input$scatter_x, " vs. ", input$scatter_y), 
+                      x = input$scatter_x, y = input$scatter_y),
+             source = "A"
+    )
+  })
+  
+  #Render scatterplot info table based on user click
+  output$clickevent <- renderTable({
+    if (length(event_data("plotly_click")) > 0) {
+      event_data <- event_data("plotly_click")
+      wdbc %>% filter((eval(as.symbol(input$scatter_x)) == event_data$x) & (eval(as.symbol(input$scatter_y)) == event_data$y))
+    } else {
+      wdbc[0, ]
+    }
+    #event_data <- event_data("plotly_click")
+    #wdbc %>% filter((eval(as.symbol(input$scatter_x)) == event_data$x) & (eval(as.symbol(input$scatter_y)) == event_data$y))
+  })
+  
+  #Render PCA biplot
+  output$biplot <- renderPlotly({
+    PCs <- prcomp(wdbc[, 3:32], center = TRUE, scale = TRUE)
+    if (input$color_diag_pca) {
+      a <- autoplot(PCs, data = wdbc, x = as.numeric(input$pca_x), y = as.numeric(input$pca_y), 
+                    colour = "diagnosis", loadings = TRUE, loadings.colour = 'black', 
+                    loadings.label = TRUE, loadings.label.size = 5, loadings.label.colour = 'black')
+    } else {
+      a <- autoplot(PCs, data = wdbc, x = as.numeric(input$pca_x), y = as.numeric(input$pca_y), 
+                    loadings = TRUE, loadings.colour = 'black', 
+                    loadings.label = TRUE, loadings.label.size = 5, loadings.label.colour = 'black')
+    }
+    ggplotly(a + labs(title = "Principle Component Analysis Biplot"))
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -6,7 +6,8 @@ dashboardPage(
   
   dashboardSidebar(sidebarMenu(
     menuItem("About", tabName = "about", icon = icon("archive")),
-    menuItem("Data Exploration", tabName = "explore", icon = icon("search"))
+    menuItem("Data Exploration", tabName = "explore", icon = icon("search")),
+    menuItem("Principal Component Analysis", tabName = "pca", icon = icon("eye"))
   )),
   
   dashboardBody(
@@ -40,45 +41,85 @@ dashboardPage(
           tabPanel("Boxplot for Numerical Columns",
             fluidRow(
               column(2,
-                     radioButtons("box_input", "Column:",
-                                 c("Radius (Mean)" = "radius_mean",
-                                   "Texture (Mean)" = "texture_mean",
-                                   "Perimeter (Mean)" = "perimeter_mean",
-                                   "Area (Mean)" = "area_mean",
-                                   "Smoothness (Mean)" = "smoothness_mean",
-                                   "Compactness (Mean)" = "compactness_mean",
-                                   "Concavity (Mean)" = "concavity_mean",
-                                   "Concave Points (Mean)" = "concavepoints_mean",
-                                   "Symmetry (Mean)" = "symmetry_mean",
-                                   "Fractal Dimension (Mean)" = "fractal_mean",
-                                   "Radius (Standard Error)" = "radius_se",
-                                   "Texture (Standard Error)" = "texture_se",
-                                   "Perimeter (Standard Error)" = "perimeter_se",
-                                   "Area (Standard Error)" = "area_se",
-                                   "Smoothness (Standard Error)" = "smoothness_se",
-                                   "Compactness (Standard Error)" = "compactness_se",
-                                   "Concavity (Standard Error)" = "concavity_se",
-                                   "Concave Points (Standard Error)" = "concavepoints_se",
-                                   "Symmetry (Standard Error)" = "symmetry_se",
-                                   "Fractal Dimension (Standard Error)" = "fractal_se",
-                                   "Radius (Worst)" = "radius_worst",
-                                   "Texture (Worst)" = "texture_worst",
-                                   "Perimeter (Worst)" = "perimeter_worst",
-                                   "Area (Worst)" = "area_worst",
-                                   "Smoothness (Worst)" = "smoothness_worst",
-                                   "Compactness (Worst)" = "compactness_worst",
-                                   "Concavity (Worst)" = "concavity_worst",
-                                   "Concave Points (Worst)" = "concavepoints_worst",
-                                   "Symmetry (Worst)" = "symmetry_worst",
-                                   "Fractal Dimension (Worst)" = "fractal_worst")
-                     )      
+                radioButtons("box_input", "Column:",
+                             c("Radius (Mean)" = "radius_mean",
+                               "Texture (Mean)" = "texture_mean",
+                               "Perimeter (Mean)" = "perimeter_mean",
+                               "Area (Mean)" = "area_mean",
+                               "Smoothness (Mean)" = "smoothness_mean",
+                               "Compactness (Mean)" = "compactness_mean",
+                               "Concavity (Mean)" = "concavity_mean",
+                               "Concave Points (Mean)" = "concavepoints_mean",
+                               "Symmetry (Mean)" = "symmetry_mean",
+                               "Fractal Dimension (Mean)" = "fractal_mean",
+                               "Radius (Standard Error)" = "radius_se",
+                               "Texture (Standard Error)" = "texture_se",
+                               "Perimeter (Standard Error)" = "perimeter_se",
+                               "Area (Standard Error)" = "area_se",
+                               "Smoothness (Standard Error)" = "smoothness_se",
+                               "Compactness (Standard Error)" = "compactness_se",
+                               "Concavity (Standard Error)" = "concavity_se",
+                               "Concave Points (Standard Error)" = "concavepoints_se",
+                               "Symmetry (Standard Error)" = "symmetry_se",
+                               "Fractal Dimension (Standard Error)" = "fractal_se",
+                               "Radius (Worst)" = "radius_worst",
+                               "Texture (Worst)" = "texture_worst",
+                               "Perimeter (Worst)" = "perimeter_worst",
+                               "Area (Worst)" = "area_worst",
+                               "Smoothness (Worst)" = "smoothness_worst",
+                               "Compactness (Worst)" = "compactness_worst",
+                               "Concavity (Worst)" = "concavity_worst",
+                               "Concave Points (Worst)" = "concavepoints_worst",
+                               "Symmetry (Worst)" = "symmetry_worst",
+                               "Fractal Dimension (Worst)" = "fractal_worst")
+                )
               ),
-              column(10, plotlyOutput("boxplot", width = "60%", height = "800px"))
+              column(10,
+                plotlyOutput("boxplot", width = "60%", height = "700px"),
+                br(),
+                downloadButton("downloadBoxplotData", "Download Data"),
+                br(),
+                h4("Note: Download button for the plot is the camera icon at the top of the plot")
+              )
             )
-          ) #End boxplot tab
+          ), #End boxplot tab
 
+          tabPanel("Scatterplot for Numerical Columns",
+            fluidRow(
+              column(2,
+                selectizeInput("scatter_x", "X-axis", choices = colnames(wdbc)[3:32]),
+                selectizeInput("scatter_y", "Y-axis", choices = colnames(wdbc)[3:32]),
+                checkboxInput("color_diag_scatter", h4("Color-code diagnosis"))
+              ),
+             column(10, 
+               plotlyOutput("scatterplot", width = "100%", height = "600px"),
+               br(),
+               h4("Click on any point in the plot - its information is displayed below:"),
+               box(width = NULL, status = "primary",
+                   div(style = 'overflow-x: scroll', tableOutput("clickevent"))
+               ),
+               br(),
+               h4("Note: Download button for the plot is the camera icon at the top of the plot")
+             )
+           )
+          ) #End scatterplot tab
         )
-      ) #End explore tab
+      ), #End explore tab
+      
+      tabItem(tabName = "pca",
+        fluidRow(
+          column(2,
+            selectizeInput("pca_x", "X-axis Principle Component", selected = 1, choices = seq(1,30)),
+            selectizeInput("pca_y", "Y-axis Principle Component", selected = 2, choices = seq(1,30)),
+            checkboxInput("color_diag_pca", h4("Color-code diagnosis"))
+          ),
+          column(10,
+            conditionalPanel(condition = "input.pca_x == input.pca_y", "X and Y axis values cannot be equal"),
+            conditionalPanel(condition = "input.pca_x != input.pca_y", plotlyOutput("biplot", width = "80%", height = "800px"))
+          )
+        )
+      ) #End pca tab
+      
     ) #End tabItems
   ) #End dashboardBody
 )
